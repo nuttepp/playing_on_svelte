b1ba6f4e8c19dd701574b60a0d20a040f6382eba
Nut Teppimol <nut.tepp@gmail.com>
2021-06-08 23:27:49 +0700

    changing to typescript project

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
-<script>
-	export let name;
+<script lang="ts">
+	export let name: string;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello svA {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
